1e9ae33784b6c94ac32464bf34df8620416b7b6c
Jeffrey <jeff@jjakt.monster>
2023-10-21 08:48:29 +0000

    added games endpoint

--- scraper-api/main.py
+++ scraper-api/main.py
@@ -6,6 +6,15 @@ import amp_scraper
 app = FastAPI()
 
 
-@app.get("/games")
-def read_root():
+@app.get("/servers/amp")
+def scrape_amp():
     return {"games": amp_scraper.list_games()}
+
+@app.get("/games/{name}")
+def get_game_description(name):
+    return {
+        "name": name,
+        "description": f"the description of {name}",
+        "images": ["https://media.giphy.com/media/nw01v68VBElqkxppKa/giphy.gif"],
+        "videos": ["https://www.youtube.com/watch?v=ESOjt2_yJrU"]
+    }
